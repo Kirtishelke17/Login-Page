d5d2fe61e1aa9f3ddbf552e9923fc6e928897d4c
Kirtishelke17 <kirtishelke3004@gmail.com>
2024-05-16 22:04:01 +0530

    create a chat section

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.blue,
       ),
       home: FormScreen(),
       routes: {
